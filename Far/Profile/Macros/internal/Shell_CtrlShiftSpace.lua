25d0ee832030aaffbf048f19203cf9586579ee80
Andrew Foster <afoster1@gmail.com>
2019-07-10 23:00:23 +0100

    Include Far macros to present the file context menu

---
+++ Far/Profile/Macros/internal/Shell_CtrlShiftSpace.lua
@@ -0,0 +1,6 @@
+Macro {
+  description="";
+  area="Shell"; key="CtrlShiftSpace";
+  flags="";
+  code="Keys(\"r c l k _ g u i : Enter\")";
+}
---
+++ Far/Profile/Macros/internal/Shell_CtrlSpace.lua
@@ -0,0 +1,6 @@
+Macro {
+  description="";
+  area="Shell"; key="CtrlSpace";
+  flags="";
+  code="Keys(\"r c l k _ t x t : Enter\")";
+}
